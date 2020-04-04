288866c6358f61ee184b90226e9b57a7e4b7139d
thanit456 <too_w_o@hotmail.com>
2020-04-04 20:55:59 +0700

    count down to new year 2020 : day1

---
+++ countdown-timer/start.js
@@ -0,0 +1,29 @@
+(() => {
+    const SECOND = 1000;
+    const MINUTE = SECOND * 60;
+    const HOUR = MINUTE * 60;
+    const DAY = HOUR * 60;
+
+    function setElementInnerText(id, text) {
+        const element = document.getElementById(id);
+        element.innerText = text;
+    }
+
+    function countdown() {
+        const now = new Date().getTime();
+        const newYear = new Date('December 31, 2020 23:59:59').getTime();
+        const unixTimeLeft = newYear - now;
+
+        setElementInnerText('days', Math.floor(unixTimeLeft / DAY));
+        setElementInnerText('hours', Math.floor(unixTimeLeft % DAY / HOUR));
+        setElementInnerText('minutes', Math.floor(unixTimeLeft % HOUR / MINUTE));
+        setElementInnerText('seconds', Math.floor(unixTimeLeft % MINUTE / SECOND));
+    
+    }
+
+    function run() {
+        countdown();
+        setInterval(countdown, SECOND);
+    }
+    run();
+})();
